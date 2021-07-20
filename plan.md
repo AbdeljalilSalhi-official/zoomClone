963b09b1ab403fc67acc09c95d7ef2bce10fe598
Abdeljalil Salhi <abdousalhi868@gmail.com>
2021-07-20 20:29:28 +0100

    Video Call App

---
+++ plan.md
@@ -0,0 +1,16 @@
+# Plan of Action
+- Initialize the NodeJS Project
+- Initialize the first view Front-end
+- Create Room ID
+- Add Ability to view Own Video
+- Add Ability to others Stream their Videos
+- Add Styling
+- Add Ability to Create Messages
+- Add Mute Button
+- Add Stop Video Button
+
+# CMDs
+$npm init
+$npm install express
+$npm install ejs
+$npm install uuid
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express'); // NodeJS Framework
+const app = express(); // Init the EXPRESS App
+const server = require('http').Server(app); // Init the HTTP Server with the EXPRESS App
+const port = process.env.PORT || 8080; // Define the Port
+const io = require('socket.io')(server); // Init the Socket.io Library
+const { v4: uuidv4 } = require('uuid'); // Unique ID
+const { ExpressPeerServer } = require('peer'); // Include PeerJS
+const peerServer = ExpressPeerServer(server, { // Initialize the PeerJS Server
+    debug: true
+});
+
+server.listen(port); // Port to Launch the Server ON
+console.log('Listening on localhost:' + port);
+
+app.set('view engine', 'ejs'); // Use EJS
+app.use(express.static('public'));
+app.use('/peerjs', peerServer); // Use PeerJS
+
+app.get('/', (req, res) => { // Get the files for App e.i. '/'=root
+    res.redirect(`/${uuidv4()}`); // Generate Unique ID (uuidv4)
+});
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room }); // Get Room ID in URL
+});
+
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId);
+        socket.broadcast.to(roomId).emit('user-connected', userId);
+        
+        socket.on('message', message => {
+            io.to(roomId).emit('createMessage', message)
+        });
+    });
+});
