e7ca81265f35c15cb7ac1b88128884686e7296f6
Abdeljalil Salhi <abdousalhi868@gmail.com>
2022-02-22 14:36:11 +0100

    fix merge bug

--- server.js
+++ server.js
@@ -1,82 +1,42 @@
-<<<<<<< HEAD
-const express = require('express'); // NodeJS Framework
+const express = require("express"); // NodeJS Framework
 const app = express(); // Init the EXPRESS App
-const server = require('http').Server(app); // Init the HTTP Server with the EXPRESS App
+const server = require("http").Server(app); // Init the HTTP Server with the EXPRESS App
 const port = process.env.PORT || 8080; // Define the Port
-const io = require('socket.io')(server); // Init the Socket.io Library
-const { v4: uuidv4 } = require('uuid'); // Unique ID
-const { ExpressPeerServer } = require('peer'); // Include PeerJS
-const peerServer = ExpressPeerServer(server, { // Initialize the PeerJS Server
-    debug: true
+const io = require("socket.io")(server); // Init the Socket.io Library
+const { v4: uuidv4 } = require("uuid"); // Unique ID
+const { ExpressPeerServer } = require("peer"); // Include PeerJS
+const peerServer = ExpressPeerServer(server, {
+  // Initialize the PeerJS Server
+  debug: true,
 });
 
 server.listen(port); // Port to Launch the Server ON
-console.log('Listening on localhost:' + port);
+console.log("Listening on localhost:" + port);
 
-app.set('view engine', 'ejs'); // Use EJS
-app.use(express.static('public'));
-app.use('/peerjs', peerServer); // Use PeerJS
+app.set("view engine", "ejs"); // Use EJS
+app.use(express.static("public"));
+app.use("/peerjs", peerServer); // Use PeerJS
 
-app.get('/', (req, res) => { // Get the files for App e.i. '/'=root
-    res.redirect(`/${uuidv4()}`); // Generate Unique ID (uuidv4)
+app.get("/", (req, res) => {
+  // Get the files for App e.i. '/'=root
+  res.redirect(`/${uuidv4()}`); // Generate Unique ID (uuidv4)
 });
 
-app.get('/:room', (req, res) => {
-    res.render('room', { roomId: req.params.room }); // Get Room ID in URL
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room }); // Get Room ID in URL
 });
 
-io.on('connection', socket => {
-    socket.on('join-room', (roomId, userId) => {
-        socket.join(roomId);
-        socket.broadcast.to(roomId).emit('user-connected', userId);
-        
-        socket.on('message', message => {
-            io.to(roomId).emit('createMessage', message)
-        });
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.broadcast.to(roomId).emit("user-connected", userId);
 
-        socket.on('disconnect', () => {
-            socket.broadcast.to(roomId).emit('user-disconnected', userId)
-        });
+    socket.on("message", (message) => {
+      io.to(roomId).emit("createMessage", message);
     });
-=======
-const express = require('express'); // NodeJS Framework
-const app = express(); // Init the EXPRESS App
-const server = require('http').Server(app); // Init the HTTP Server with the EXPRESS App
-const port = process.env.PORT || 8080; // Define the Port
-const io = require('socket.io')(server); // Init the Socket.io Library
-const { v4: uuidv4 } = require('uuid'); // Unique ID
-const { ExpressPeerServer } = require('peer'); // Include PeerJS
-const peerServer = ExpressPeerServer(server, { // Initialize the PeerJS Server
-    debug: true
-});
 
-server.listen(port); // Port to Launch the Server ON
-console.log('Listening on localhost:' + port);
-
-app.set('view engine', 'ejs'); // Use EJS
-app.use(express.static('public'));
-app.use('/peerjs', peerServer); // Use PeerJS
-
-app.get('/', (req, res) => { // Get the files for App e.i. '/'=root
-    res.redirect(`/${uuidv4()}`); // Generate Unique ID (uuidv4)
-});
-
-app.get('/:room', (req, res) => {
-    res.render('room', { roomId: req.params.room }); // Get Room ID in URL
-});
-
-io.on('connection', socket => {
-    socket.on('join-room', (roomId, userId) => {
-        socket.join(roomId);
-        socket.broadcast.to(roomId).emit('user-connected', userId);
-        
-        socket.on('message', message => {
-            io.to(roomId).emit('createMessage', message)
-        });
-
-        socket.on('disconnect', () => {
-            socket.broadcast.to(roomId).emit('user-disconnected', userId)
-        });
+    socket.on("disconnect", () => {
+      socket.broadcast.to(roomId).emit("user-disconnected", userId);
     });
->>>>>>> 71aca75795c22f5d8d650c123b7438c3888ec324
+  });
 });
